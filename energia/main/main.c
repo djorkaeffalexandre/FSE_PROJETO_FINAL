7139b9e970603b505dca19c5819cabe7100776ec
Djorkaeff Alexandre <djorkaeff.unb@gmail.com>
2021-05-12 23:42:45 -0300

    Adicionando leitura de DHT11

--- energia/main/main.c
+++ energia/main/main.c
@@ -5,10 +5,13 @@
 #include "freertos/task.h"
 #include "driver/gpio.h"
 
+#include "dht11.h"
+
 #define LED 2
 
 void app_main(void)
 {
+  DHT11_init(GPIO_NUM_4);
   gpio_pad_select_gpio(LED);
   gpio_set_direction(LED, GPIO_MODE_OUTPUT);
   
@@ -16,6 +19,9 @@ void app_main(void)
 
   while (1)
   {
+    printf("Temperature is %d \n", DHT11_read().temperature);
+    printf("Humidity is %d\n", DHT11_read().humidity);
+    printf("Status code is %d\n", DHT11_read().status);
     gpio_set_level(LED, state);
     vTaskDelay(3000 / portTICK_PERIOD_MS);
     state = !state;
